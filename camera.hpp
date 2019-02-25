da50fe37384ef8afd1dbed21cc07e6cb36a4669b
visidh <s8visidh@stud.uni-saarland.de>
2019-02-25 14:33:11 +0100

    implemented camera, antialising

---
+++ camera.hpp
@@ -0,0 +1,25 @@
+#ifndef CAMERA
+#define CAMERA
+
+# include "ray.hpp"
+
+class camera{
+	public:
+		vec3 origin;
+		vec3 lower_left_corner_img_plane;
+		vec3 horizontal;
+		vec3 vertical;
+
+		camera() {
+			lower_left_corner_img_plane = vec3(-2.0, -1.0, -1.0);
+			origin = vec3(0.0, 0.0, 0.0);
+			horizontal = vec3(4.0, 0.0, 0.0);
+			vertical = vec3(0.0, 2.0, 0.0);
+		}
+
+		ray get_ray(float u, float v) {
+			return ray(origin, lower_left_corner_img_plane + u*horizontal + v*vertical - origin);
+		}
+};
+
+#endif
